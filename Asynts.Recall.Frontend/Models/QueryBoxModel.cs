b99c48e3571e9f299b0749bd587074d14900a5c1
Paul Scharnofske <asynts@gmail.com>
2023-02-15 11:15:27 +0100

    Move query text boxes into 'QueryBox' and parse query

---
+++ Asynts.Recall.Frontend/Models/QueryBoxModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using Asynts.Recall.Backend.Persistance.Data;
+
+namespace Asynts.Recall.Frontend.Models;
+
+internal partial class QueryBoxModel : ObservableObject
+{
+    [ObservableProperty]
+    private string query = string.Empty;
+
+    [ObservableProperty]
+    private string rawQuery = string.Empty;
+
+    [RelayCommand]
+    private void SubmitQuery()
+    {
+        var searchQueryData = ParseQuery();
+
+        // FIXME: Do something.
+        Debug.WriteLine("Updating query!");
+    }
+
+    private SearchQueryData ParseQuery()
+    {
+        var interestingTerms = new List<string>();
+        var requiredTags = new List<string>();
+
+        var queryParts = Query.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        foreach (var queryPart in queryParts)
+        {
+            if (queryPart.StartsWith('[') && queryPart.EndsWith(']')) {
+                requiredTags.Add(queryPart.Substring(1, queryPart.Length - 2));
+            } else
+            {
+                interestingTerms.Add(queryPart);
+            }
+        }
+
+        return new SearchQueryData
+        {
+            InterestingTerms = interestingTerms,
+            RequiredTags = requiredTags,
+            RawTextQuery = RawQuery,
+        };
+    }
+}
---
+++ Asynts.Recall.Frontend/QueryBox.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+using Asynts.Recall.Frontend.Models;
+
+namespace Asynts.Recall.Frontend
+{
+    /// <summary>
+    /// Interaction logic for QueryBox.xaml
+    /// </summary>
+    public partial class QueryBox : UserControl
+    {
+        private readonly QueryBoxModel _queryBoxModel;
+
+        public QueryBox()
+        {
+            InitializeComponent();
+
+            _queryBoxModel = new QueryBoxModel();
+            DataContext = _queryBoxModel;
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                _queryBoxModel.SubmitQueryCommand.Execute(null);
+            }
+        }
+    }
+}
